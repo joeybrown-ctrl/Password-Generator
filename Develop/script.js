076690fa65a8fe5cfa2e174ca18eb5f866e0e6c2
Joey Brown <gjoey.brown@gmail.com>
2020-08-27 12:58:28 -0700

    refactored arrays and defined variables adding in the .split method

--- Develop/script.js
+++ Develop/script.js
@@ -17,55 +17,43 @@ function writePassword() {
  * generate a password based on certain criteria
  */
 function generatePassword(){
+ 
+  let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerChar = "abcdefghijklmnopqrstuvwxyz";
+  let numChar = "0123456789";
+  let specChar = "!@#$%^&*-+=(){}[]<>?~";
 
-  let pwLength = prompt("Choose a password length between 8 and 128 characters");
-  let lowerCase = confirm("Would you like your password to contain lowercase letters?");
-  let UpperCase = confirm("Would you like your password to contain uppercase letters?");
-  let numeric = confirm("Would you like your password to contain numbers?");
-  let specChar = confirm("Would you like your password to contain special characters?");
-
-  if(pwLength >= "8" && pwLength <= "128") {
-
-  };
-
-  if(lowerCase === true) {
-
-  } else {
-
-  };
-
-  if(UpperCase === true) {
-
-  } else {
-
-  };
-
-  if(numeric === true) {
-
-  } else {
-
-  };
-
-  if(specChar === true) {
-
-  } else {
-
-  };
+  let upperCharArr = upperChar.split("");
+  let lowerCharArr = lowerChar.split("");
+  let numCharArr = numChar.split("");
+  let specCharArr = specChar.split("");
 
 
-  let uCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let lCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let numChar =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
-  let sChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", "<", ">", "?", "/", "~", "[", "]", "{", "}"];
+
 
 
 
   // Password is currently blank! We need to make a better one
   let password = "";
+  let charArr [];
 
+  let pwLength = prompt("Choose a password length between 8 and 128 characters");
+  let lowerCase = confirm("Would you like your password to contain lowercase letters?");
+  let UpperCase = confirm("Would you like your password to contain uppercase letters?");
+  let numeric = confirm("Would you like your password to contain numbers?");
+  let special = confirm("Would you like your password to contain special characters?");
 
+  if (pwLength < 8 || pwlength > 128) {
+    alert("Password length must be between 8 and 128 characters");
+  };
 
+  if (UpperCase === true) {
+    Array.prototype.apply(charArr, upperChar);
+  }
 
+  if (lowerCase === true) {
+    Array.prototype.apply(charArr, lowerChar);
+  }
 
   return password;
 }
