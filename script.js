75a37429d490dadefabdfa55324d9ba174a5e9a8
Joey Brown <gjoey.brown@gmail.com>
2020-08-29 19:39:45 -0700

    added inline comments to explain my process.

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function writePassword() {
  * generate a password based on certain criteria
  */
 function generatePassword(){
- 
+ //assigned character strings to variables
   let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let lowerChar = "abcdefghijklmnopqrstuvwxyz";
   let numChar = "0123456789";
   let specChar = "!@#$%^&*_+=()<>/?`~";
-
+//used .split() method to split those strings into character arrays
   let upperCharArr = upperChar.split("");
   let lowerCharArr = lowerChar.split("");
   let numCharArr = numChar.split("");
@@ -31,10 +31,11 @@ function generatePassword(){
 
   // Password is currently blank! We need to make a better one
   let password = "";
+//assigned this empty array to be used later
   let charArr = [];
-
+//prompting the user to choose a password length
   let pwLength = prompt("Choose a password length between 8 and 128 characters");
-
+//used if statements to ensure that the user enters a number of the correct length
   if (pwLength < 8 || pwLength > 128) {
     alert("Password length must be between 8 and 128 characters. Please click OK and refresh browser.");
     return 
@@ -44,13 +45,13 @@ function generatePassword(){
     alert("Please provide password length as a number.  Please click OK and refresh browser.");
     return
   }
-
+//confirming which character types the user would like to choose
   let lowerCase = confirm("Would you like your password to contain lowercase letters?");
   let upperCase = confirm("Would you like your password to contain uppercase letters?");
   let numeric = confirm("Would you like your password to contain numbers?");
   let special = confirm("Would you like your password to contain special characters?");
 
-
+//executing this logic using if statements and the .concat() method
   if (upperCase === true) {
     charArr = charArr.concat(upperCharArr)
   }
@@ -66,17 +67,17 @@ function generatePassword(){
   if (special === true) {
     charArr = charArr.concat(specCharArr)
   }
-
+//final condition check ensuring user has selected at least one character type
   if (charArr.length === 0) {
     alert("One or more of the character types must be selected");
-  }
+  }//for loop creating the random password
     else {
       for (let i = 0; i < pwLength; i++) {
       let pwRandom = Math.floor(Math.random() * charArr.length);
       password = password + charArr[pwRandom];
       }
     }
-
+//returning the password!
   return password;
 }
 
