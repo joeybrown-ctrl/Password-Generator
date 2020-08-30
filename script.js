2153125dd444452ffa3517a05e6c004111051957
Joey Brown <gjoey.brown@gmail.com>
2020-08-29 19:58:46 -0700

    cleaned up my code for readability, added whitespace, etc.

--- script.js
+++ script.js
@@ -17,24 +17,28 @@ function writePassword() {
  * generate a password based on certain criteria
  */
 function generatePassword(){
+
  //assigned character strings to variables
   let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let lowerChar = "abcdefghijklmnopqrstuvwxyz";
   let numChar = "0123456789";
   let specChar = "!@#$%^&*_+=()<>/?`~";
+  
 //used .split() method to split those strings into character arrays
   let upperCharArr = upperChar.split("");
   let lowerCharArr = lowerChar.split("");
   let numCharArr = numChar.split("");
   let specCharArr = specChar.split("");
 
-
   // Password is currently blank! We need to make a better one
   let password = "";
+
 //assigned this empty array to be used later
   let charArr = [];
+
 //prompting the user to choose a password length
   let pwLength = prompt("Choose a password length between 8 and 128 characters");
+
 //used if statements to ensure that the user enters a number of the correct length
   if (pwLength < 8 || pwLength > 128) {
     alert("Password length must be between 8 and 128 characters. Please click OK and refresh browser.");
@@ -44,7 +48,8 @@ function generatePassword(){
   if (isNaN(pwLength) === true) {
     alert("Please provide password length as a number.  Please click OK and refresh browser.");
     return
-  }
+  };
+
 //confirming which character types the user would like to choose
   let lowerCase = confirm("Would you like your password to contain lowercase letters?");
   let upperCase = confirm("Would you like your password to contain uppercase letters?");
@@ -53,19 +58,19 @@ function generatePassword(){
 
 //executing this logic using if statements and the .concat() method
   if (upperCase === true) {
-    charArr = charArr.concat(upperCharArr)
+    charArr = charArr.concat(upperCharArr);
   }
 
   if (lowerCase === true) {
-    charArr = charArr.concat(lowerCharArr)
+    charArr = charArr.concat(lowerCharArr);
   }
 
   if (numeric === true) {
-    charArr = charArr.concat(numCharArr)
+    charArr = charArr.concat(numCharArr);
   }
 
   if (special === true) {
-    charArr = charArr.concat(specCharArr)
+    charArr = charArr.concat(specCharArr);
   }
 //final condition check ensuring user has selected at least one character type
   if (charArr.length === 0) {
@@ -80,8 +85,6 @@ function generatePassword(){
 //returning the password!
   return password;
 }
-
-                 
-
+          
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
